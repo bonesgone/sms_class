c19824dfb5f873cc4622bbf84161275de572b5c0
bonesgone <ulantemirlan2002@gmail.com>
2023-03-27 00:00:03 +0900

    Homework_1

---
+++ catkin_ws/src/project_ws4/src/subscriber_node.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void writeMsgToLog(const std_msgs::String::ConstPtr &msg){
+    ROS_INFO("The message we received: %s", msg->data.c_str());
+    
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "Subscriber");
+    ros::NodeHandle nh;
+
+    ros::Subscriber topic_sub = nh.subscribe("tutorial", 1000, writeMsgToLog);
+
+    ros::spin();
+
+    return 0;
+}
